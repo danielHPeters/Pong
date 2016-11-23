505709ae92a03a286708750bf1ecbc574d8f51c7
d.peters <d.peters@PC0185.Stiftung.ifa>
2016-11-23 14:16:35 +0100

    moved initializers to constuctor

--- src/pong/Main.java
+++ src/pong/Main.java
@@ -17,58 +17,58 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     private final int dimension = 500;
-    
+
     /**
      * the game loop
      */
-    private RunGame loop;
+    private final RunGame loop;
+
+    private final Thread executer;
 
     /**
      * the game window
      */
-    private final GameWindow window = new GameWindow(dimension);
+    private final GameWindow window;
 
     /**
      * the drawing panel containing the game loop and objects
      */
-    private final GameArea panel = new GameArea(dimension);
+    private final GameArea panel;
 
     /**
      * initialize the keybindings of the game
      */
-    GameKeyBindings gameKeyBindings = new GameKeyBindings(panel, panel.getPlayers());
+    GameKeyBindings gameKeyBindings;
 
     /**
      * This is the default constructor
      */
     public Main() {
+        this.window = new GameWindow(dimension);
+        this.panel = new GameArea(dimension);
+        this.gameKeyBindings = new GameKeyBindings(panel, panel.getPlayers());
+        this.window.add(panel, BorderLayout.CENTER);
+        this.window.pack();
+        this.loop = new RunGame(panel);
+        this.executer = new Thread(this.loop);
     }
 
     /**
-     * initializes the JFrame window
-     */
-    public void initialize() {
-        window.add(panel, BorderLayout.CENTER);
-        window.pack();
-        window.setVisible(true);
-    }
-    
-    /**
      * starts the game loop
      */
-    public void start(){
-        loop = new RunGame(panel);
-        loop.runLoop();
+    public void start() {
+        this.window.setVisible(true);
+        this.executer.start();
     }
 
     /**
      * starts the event dispatch thread and starts the game
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             Main pong = new Main();
-            pong.initialize();
             pong.start();
         });
     }
